94a0cefdbf69ac8d616df1a3ade053fed1f10694
Chyi Wang <chyiyenwang@gmail.com>
2015-10-12 15:47:08 -0700

    Can bounce off left paddle, but if it goes too fast it breaks

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
 	var canvas = document.getElementById("board");
 	var ctx = canvas.getContext("2d");
-	var raf;
 	var addE = document.addEventListener;
 
-	// Paddle
+	// Paddle constructor
 	var Paddle = function(x, y, width, height, color) {
 		this.x = x;
 		this.y = y;
@@ -13,11 +12,11 @@ $(document).ready(function() {
 		this.color = color;
 		this.draw = function() {
 			ctx.fillStyle = color;
-			ctx.fillRect(x, this.y, width, this.height);
+			ctx.fillRect(this.x, this.y, width, this.height);
 		}
 	};
 
-	// Ball
+	// Ball constructor
 	var Ball = function(x, y, vx, vy, r, color) {
 	  this.x = x;
 	  this.y = y;
@@ -44,42 +43,75 @@ $(document).ready(function() {
 	  pingPong.draw();
 	  pingPong.x += pingPong.vx;
 	  pingPong.y += pingPong.vy;
-	  if (pingPong.y == (canvas.height - 5) || pingPong.y == 5) {
+	  // bottom || top
+	  if (pingPong.y >= (canvas.height - 5) || pingPong.y <= 5) {
 	  	pingPong.vy = pingPong.vy * -1;
 	  	pingPong.y += pingPong.vy;
 	  }
-	  else if (pingPong.x == (left.x + 34)) {
+	  // collision detection line intersection
+	  else if(pingPong.x == (left.x + 34) && pingPong.y < (left.y + left.height) && pingPong.y > left.y) {;
 	  	pingPong.vx = pingPong.vx * -1;
 	  	pingPong.x += pingPong.vx;
-	  }
-	  raf = window.requestAnimationFrame(ballMovement);
+	  };
+	  // left || right
+	  // else if (pingPong.x <= (left.x + 30) || pingPong.x >= (canvas.width - 34)) {
+	  // 	pingPong.vx = pingPong.vx * -1;
+	  // 	pingPong.x += pingPong.vx;
+	  // }
+	  requestAnimationFrame(ballMovement);
 	}
 
 	// This creates the paddles and the ball with their
 	// respective attributes
 	var left = new Paddle(10, 0, 20, 150, 'red'),
 			right = new Paddle(970, 0, 20, 150, 'green'),
-			pingPong = new Ball(300, 100, -1, 5, 15, 'rgba(0,0,0,1)');
+			pingPong = new Ball(300, 100, -2, -6, 15, 'rgba(0,0,0,1)');
 	left.draw();
 	right.draw();
 	ballMovement();
 
+	// Currently this function only moves the left paddle down.  Fix this shit up, bitch!
+	var paddleMovement = function() {
+		var speed = 10;
+
+		ctx.clearRect(0, 0, 30, canvas.height);
+		left.draw();
+		left.y += speed
+		requestAnimationFrame(paddleMovement);
+	}
+
+	// Currently this function only stops the left paddle from moving down
+	var stopMovement = function() {
+		ctx.clearRect(0, 0, 30, canvas.height);
+		left.draw();
+		left.y -= 10;
+		requestAnimationFrame(stopMovement);
+	}
+
+	// Keydown function that calls paddleMovement()
 	addE('keydown', function(e) {
-		if (e.keyCode == 87) {
-			ctx.clearRect(0, 0, 30, canvas.height);
-			left.y -= 24;
-			left.height = 150;
-			left.draw();
-			console.log(left.y);
-			console.log(left.height);
+		if (e.keyCode == 83) {
+			paddleMovement();
 		}
-		else if (e.keyCode == 83) {
-			ctx.clearRect(0, 0, 30, canvas.height);
-			left.y += 24;
-			left.height = 150;
-			left.draw();
-			console.log(left.y);
-			console.log(left.height);
-		} 
+	});
+
+	// Keyup function that calls stopMovement();
+	addE('keyup', function(e) {
+		if (e.keyCode == 83) {
+			stopMovement();
+		}
 	})
+
+	// addE('keydown', function(e) {
+	// 	if (e.keyCode == 87) {
+	// 		ctx.clearRect(0, 0, 30, canvas.height);
+	// 		left.y -= 24;
+	// 		left.draw();
+	// 	}
+	// 	else if (e.keyCode == 83) {
+	// 		ctx.clearRect(0, 0, 30, canvas.height);
+	// 		left.y += 24;
+	// 		left.draw();
+	// 	} 
+	// })
 });
